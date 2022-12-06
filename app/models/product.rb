9edd7295ae01d94c483a5b846c2a940bfd55e1c6
divxouchi <manabu.ouchi@divx.co.jp>
2022-12-06 09:39:01 +0900

    バリデーションの記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+
+  validates :title, presence: true
+  validates :money, presence: true, numericality: { only_integer: true }
+  validates :content, presence: true
 end
